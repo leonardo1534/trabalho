75e7ccc06f0fb384adf0108684db11800021b83f
leonardo1534 <128159720+leonardo1534@users.noreply.github.com>
2023-10-06 12:02:36 -0300

    Add files via upload

---
+++ projetojs/menu.js
@@ -0,0 +1,17 @@
+function pedir(){
+  var opção =prompt("Escolha uma opção de 1 a 4")
+  switch(Number(opção)){
+    case 1:
+        alert("Você escolheu Qboa")
+        break;
+    case 2:
+        alert("Você escolheu Papel Higiênico")
+        break;
+    case 3: 
+        alert("Você escolheu Sabonete")
+        break;
+    case 4:
+        alert("Você esscolheu Desinfetante") 
+        break;   
+  }
+}
